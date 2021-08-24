aff47f955d17726ab5376b824b815991b25df45a
Ramazan Cinardere <ramazan.cinardere@ctream.de>
2021-08-24 22:42:40 +0200

    Initial commit

---
+++ ExtractAuthorNames.java
@@ -0,0 +1,47 @@
+package com.coneva.dic;
+
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ExtractAuthorNames {
+
+    static File authors = new File("authors.txt");
+
+    public static void main(String[] args) throws IOException {
+
+        var profileCounter = new AtomicInteger();
+        var path = Paths.get("src", "main", "resources", "profiles").toAbsolutePath();
+        Files.walk(path, FileVisitOption.FOLLOW_LINKS)
+                .filter(Files::isRegularFile)
+                .map(ExtractAuthorNames::extractAuthorNameAndBirthday)
+                .forEach(profile -> writeToFile(profile, profileCounter.incrementAndGet()));
+
+    }
+
+    @SneakyThrows
+    private static void writeToFile(String profile, int incrementAndGet) {
+        var identity = incrementAndGet + " " + profile;
+        Files.writeString(authors.toPath(), identity + System.lineSeparator(), StandardCharsets.UTF_8, CREATE, APPEND);
+    }
+
+    @SneakyThrows
+    private static String extractAuthorNameAndBirthday(Path path) {
+        return Files.readAllLines(path).stream()
+                .filter(line -> line.contains("Name") || line.contains("Date of birth"))
+                .reduce((s, s2) -> {
+                    var name = s.split("Name:")[1];
+                    var birthday = s2.split("Date of birth:")[1];
+                    return String.join(",", name, birthday);
+                }).orElse("Not found");
+    }
+
+
+}
---
+++ exercise.md
@@ -0,0 +1,22 @@
+### Scenario:
+You work in a market research institute as a data analyst. 
+Every day you are sent various profiles, the content of which is to be analysed by you. 
+These profiles are stored in a public directory. 
+The profile must be in a format specified by you so that you can read and analyse the content without further ado.
+
+
+    # Header
+    Name: <prename, surname>
+    Date of birth: <date of birth>
+
+    # Body
+    <content>
+
+#### Task:
+Your task is to read the profiles programmatically and extract the names and dates of birth of the individual authors, and write it into a separate file (autors.txt). It is important that only one author is listed per line.
+
+#### Example
+    1. Markus Mustermann, 1986-09-01
+    2. Martina Müller, 1999-10-10
+    ...
+
